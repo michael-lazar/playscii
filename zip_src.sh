5dd6373ee18f98f7143432a30c43fb991a6ab2da
jplebreton <jplebreton@localhost>
2015-02-23 17:36:38 -0800

    added bash script for building source distributions

---
+++ zip_src.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# builds a source distribution
+rm playscii_source.zip
+zip -ur9 playscii_source.zip . -i '*.py' 'charsets/*' 'palettes/*' 'scripts/*.arsc' 'shaders/*.glsl' 'ui/*.png' 'readme.txt' '*.default' 'license.txt' '*.bat' 'zip_src.sh' 'art/hello2.psci' 'art/owell.ed'
