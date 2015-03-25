476ee69a5a304e39ae510f8412c864d364921839
Sean Gubelman <sean.gubelman@gmail.com>
2015-03-25 12:57:33 -0700

    Update shaders to v150 for mac compat

--- shaders/renderable_f.glsl
+++ shaders/renderable_f.glsl
@@ -1,4 +1,4 @@
-#version 130
+#version 150
 
 uniform sampler2D charset;
 uniform sampler2D palette;
@@ -21,12 +21,12 @@ void main()
 {
 	// add tiny offsets to UVs to account for sampling imprecision
 	vec2 nudge = vec2(0.000001, 0.000001);
-	outColor = texture2D(charset, texCoords + nudge);
+	outColor = texture(charset, texCoords + nudge);
 	// look up fg/bg colors from palette texture
 	vec2 colorUV = vec2(0.0, 0.0);
 	// offset U coord slightly so we're not sampling from pixel boundary
 	colorUV.x = (theFgColorIndex + 0.01) / palTextureWidth;
-	vec4 fgColor = texture2D(palette, colorUV);
+	vec4 fgColor = texture(palette, colorUV);
 	// multiple charset pixel value by FG color
 	// tinting >1 color charsets isn't officially supported but hey
 	outColor.rgb *= fgColor.rgb;
@@ -37,7 +37,7 @@ void main()
 	// thanks Mark Wonnacott for tip on how to do this w/o a branch
 	outColor = mix(bgColor, fgColor, outColor.a);
 	// apply "grain" for eg UI elements
-	vec4 grainColor = texture2D(grain, gl_FragCoord.xy * grainSize);
+	vec4 grainColor = texture(grain, gl_FragCoord.xy * grainSize);
 	outColor.rgb += (0.5 - grainColor.rgb) * grainStrength;
 	// overall alpha
 	outColor.a *= alpha;
