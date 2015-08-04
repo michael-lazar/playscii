a450de8415e487c3e2d8bf01dbe42b44857fb2c3
jplebreton <jplebreton@localhost>
2015-08-04 16:09:03 -0700

    - add a couple files to source zip script

--- zip_src.sh
+++ zip_src.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # builds a source distribution
 rm playscii_source.zip
-zip -ur9 playscii_source.zip . -i '*.py' 'charsets/*' 'palettes/*' 'scripts/*.arsc' 'shaders/*.glsl' 'ui/*.png' 'README.md' 'profile' '*.default' 'license.txt' 'code_of_conduct.txt' '*.bat' 'zip_src.sh' 'art/*' 'games/*.*' -x '*/__pycache__*'
+zip -ur9 playscii_source.zip . -i '*.py' 'charsets/*' 'palettes/*' 'scripts/*.arsc' 'shaders/*.glsl' 'ui/*.png' 'README.md' 'profile' '*.default' 'license.txt' 'code_of_conduct.txt' '*.bat' 'zip_src.sh' 'art/*' 'games/*.*' 'requirements.txt' 'Info.plist' -x '*/__pycache__*'
