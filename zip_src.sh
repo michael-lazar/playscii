e2121292636214ddb7c6d0ca1ee850dfd7827d51
jplebreton <jplebreton@localhost>
2015-08-03 18:46:35 -0700

    - use Application.find_filename_path for images to be converted - fixes to Application.find_filename_path for "no subdir or extensions" case, better support for relative paths - fixes/updates to source build script

--- zip_src.sh
+++ zip_src.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # builds a source distribution
 rm playscii_source.zip
-zip -ur9 playscii_source.zip . -i '*.py' 'charsets/*' 'palettes/*' 'scripts/*.arsc' 'shaders/*.glsl' 'ui/*.png' 'README.md' 'profile' '*.default' 'license.txt' 'code_of_conduct.txt' '*.bat' 'zip_src.sh' 'art/hello2.psci' 'art/owell.ed' 'art/new.psci' 'art/blob_shadow.psci' 'games/test1/*.*' 'games/cronotest/*.*'
+zip -ur9 playscii_source.zip . -i '*.py' 'charsets/*' 'palettes/*' 'scripts/*.arsc' 'shaders/*.glsl' 'ui/*.png' 'README.md' 'profile' '*.default' 'license.txt' 'code_of_conduct.txt' '*.bat' 'zip_src.sh' 'art/*' 'games/*.*' -x '*/__pycache__*'
