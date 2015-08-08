51b2ddec0ce330e02d50af89941b65bca863c1c9
jplebreton <jplebreton@localhost>
2015-08-08 09:38:44 -0700

    - source zip build script uses a top containing folder, like any polite project should - use include file list for source zip builds

--- zip_src.sh
+++ zip_src.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 # builds a source distribution
 rm playscii_source.zip
-zip -ur9 playscii_source.zip . -i '*.py' 'charsets/*' 'palettes/*' 'scripts/*.arsc' 'shaders/*.glsl' 'ui/*.png' 'README.md' 'profile' '*.default' 'license.txt' 'code_of_conduct.txt' '*.bat' 'zip_src.sh' 'art/*' 'games/*.*' 'requirements.txt' 'Info.plist' -x '*/__pycache__*'
+zip -ur9 playscii_source.zip . -i@zip_src_include -x '*/__pycache__*'
+# put all this in a containing directory
+# TODO: this feels like a super kludgy way to do this, find out a better way
+mkdir playscii
+cd playscii
+unzip ../playscii_source.zip
+cd ..
+rm playscii_source.zip
+zip -ur9 playscii_source.zip playscii/*
+rm -rf playscii/
