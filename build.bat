bbad071d648ed691f887e8a0142eb6b2d325a2a9
jplebreton <jplebreton@localhost>
2015-10-05 23:21:49 -0700

    - new build batch files for windows build - icon for windows exe

--- build.bat
+++ build.bat
@@ -2,52 +2,14 @@
 
 REM Playscii windows EXE build batch
 
-REM Build needs to know where your python and dlls are, modify
-REM the two lines below as needed:
-set PYTHONPATH="c:\Python34"
+REM Build needs to know where your dlls are, modify line below as needed:
 set DLLPATH=".\"
 
-echo Removing old build...
-rmdir /S /Q dist\
-mkdir dist
-
 echo Creating new build...
-python setup.py py2exe
-
-echo Copying PyOpenGL libs to build dir...
-mkdir dist\OpenGL
-xcopy /e /v /Q "%PYTHONPATH%\Lib\site-packages\OpenGL\*.*" dist\OpenGL\
-del /s /Q dist\OpenGL\*.pyc
-del /s /Q dist\OpenGL\*.pyo
+pyinstaller playscii.spec --icon-file=ui\playscii.ico
 
 echo Copying DLLs...
-copy %DLLPATH%\*.dll dist\
-
-echo Copying Playscii data files...
-mkdir dist\art
-copy /v art\*.* dist\art\
-mkdir dist\charsets
-copy /v charsets\*.* dist\charsets\
-REM mkdir dist\docs
-REM copy /v docs\*.* dist\docs\
-mkdir dist\palettes
-copy /v palettes\*.* dist\palettes\
-mkdir dist\scripts
-copy /v scripts\*.* dist\scripts\
-mkdir dist\shaders
-copy /v shaders\*.* dist\shaders\
-REM test game content
-mkdir dist\games
-xcopy /s games\*.* dist\games\
-REM ignore ui art source assets (eg .xcf)
-mkdir dist\ui
-copy /v ui\*.png dist\ui\
-copy README.md dist\
-copy license.txt dist\
-copy code_of_conduct.txt dist\
-copy playscii.cfg.default dist\
-copy binds.cfg.default dist\
-copy version dist\
+copy %DLLPATH%\*.dll dist\playscii
 
 echo Done!
 pause
--- zip_build.bat
+++ zip_build.bat
@@ -3,12 +3,5 @@ set ZIP_EXE="c:\Program Files\7-Zip\7z.exe"
 REM get version number from file
 set /p PLAYSCII_VERSION=<version
 del playscii_win32*.zip
-REM ignore pycache dirs
-dir /s/b dist\*__pycache__ > cachedirs
-REM rename dist\ dir to the top level dir we want in the archive
-move dist playscii
 REM name zip according to version
-%ZIP_EXE% a -r playscii_win32-%PLAYSCII_VERSION%.zip playscii/* -x@cachedirs
-REM put everything back
-move playscii dist
-del cachedirs
+%ZIP_EXE% a -r playscii_win32-%PLAYSCII_VERSION%.zip dist/*
