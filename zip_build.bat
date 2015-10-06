bf90e43b452591128849c149956e40db6246b1e1
jplebreton <jplebreton@localhost>
2015-10-05 23:32:52 -0700

    fixes to windows build batchfiles

--- zip_build.bat
+++ zip_build.bat
@@ -2,6 +2,9 @@
 set ZIP_EXE="c:\Program Files\7-Zip\7z.exe"
 REM get version number from file
 set /p PLAYSCII_VERSION=<version
-del playscii_win32*.zip
+cd dist
+del /Q playscii_win32*.zip
 REM name zip according to version
-%ZIP_EXE% a -r playscii_win32-%PLAYSCII_VERSION%.zip dist/*
+%ZIP_EXE% a -r playscii_win32-%PLAYSCII_VERSION%.zip ./*
+move playscii_win32-%PLAYSCII_VERSION%.zip ..\
+cd ..
