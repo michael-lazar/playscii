da8ee479b345021a5a33de1954e906004fb21487
jplebreton <jplebreton@localhost>
2015-10-17 13:26:40 -0700

    - pyinstaller spec file for windows builds

--- build_windows.bat
+++ build_windows.bat
@@ -1,15 +1,7 @@
 @echo off
 
-REM Playscii windows EXE build batch
-
-REM Build needs to know where your dlls are, modify line below as needed:
-set DLLPATH=".\"
-
 echo Creating new build...
-pyinstaller --icon=ui\playscii.ico playscii.spec
-
-echo Copying DLLs...
-copy %DLLPATH%\*.dll dist\playscii
+pyinstaller --icon=ui\playscii.ico playscii_win.spec
 
 echo Done!
 pause
