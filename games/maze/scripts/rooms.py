d30bfdd74c862fb1dd26f3d5ad4665f178c42998
jplebreton <jplebreton@localhost>
2015-10-24 16:56:59 -0700

    - code-driven anim fx for portal gate and key - clear HUD message when changing screens (MazeRoom class) - added empty heart and keyhole to JPETSCII set! - added MazeBG class, z -0.1 by default - "used" message for objects

--- games/maze/scripts/rooms.py
+++ games/maze/scripts/rooms.py
@@ -1,16 +1,26 @@
 
 from game_room import GameRoom
 
-class OutsideRoom(GameRoom):
+
+class MazeRoom(GameRoom):
+    
+    def exited(self, new_room):
+        GameRoom.exited(self, new_room)
+        # clear message line when exiting
+        if self.world.hud:
+            self.world.hud.post_msg('')
+
+
+class OutsideRoom(MazeRoom):
     
     camera_follow_player = True
     
     def entered(self, old_room):
-        GameRoom.entered(self, old_room)
+        MazeRoom.entered(self, old_room)
         self.world.collision_enabled = False
         self.world.app.camera.y_tilt = 4
     
     def exited(self, new_room):
-        GameRoom.exited(self, new_room)
+        MazeRoom.exited(self, new_room)
         self.world.collision_enabled = True
         self.world.app.camera.y_tilt = 0
