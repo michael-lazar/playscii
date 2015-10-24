4ae8be40356c1093d82a84ffa4562aeb08099900
jplebreton <jplebreton@localhost>
2015-10-23 19:33:16 -0700

    - collision: don't call stopped_colliding if other isn't in object's contacts dict, but research why this happens - added GameRoom.camera_follow_player -- added GameWorld.enabled/disable player camera lock functions - created GameCamera subclass for any defaults that work better for game mode - message line gives feedback when object is click-spawned - removed world-to-screen debug element - stubbed out rooms and progression of maze game, let's call it an alpha :]

--- games/maze/scripts/player.py
+++ games/maze/scripts/player.py
@@ -9,3 +9,26 @@ class MazePlayer(Player):
     def __init__(self, world, obj_data=None):
         Player.__init__(self, world, obj_data)
         self.held_object = None
+    
+    def pick_up(self, pickup):
+        # drop any other held item first
+        if self.held_object:
+            self.drop(self.held_object, pickup)
+        self.held_object = pickup
+        pickup.holder = self
+        self.world.hud.post_msg('got %s!' % pickup.display_name)
+        pickup.disable_collision()
+    
+    def drop(self, pickup, new_pickup=None):
+        # drop pickup in place of one we're swapping with, else drop at feet
+        if new_pickup:
+            pickup.x, pickup.y = new_pickup.x, new_pickup.y
+        else:
+            pickup.x, pickup.y = self.x, self.y
+        pickup.holder = None
+    
+    def use_item(self):
+        if self.held_object.consume_on_use:
+            obj, self.held_object = self.held_object, None
+            obj.holder = None
+            obj.destroy()
