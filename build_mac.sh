5f7ef348745b1611fe9e660ebe71fbebe4aa119b
jplebreton <jplebreton@localhost>
2015-10-24 09:37:01 -0700

    - fixes and streamlining for mac build: manually create SDL2 symlinks to libs, auto-create dmg from app bundle and move it to dropbox folder. if anyone besides me needs to use this script i can remove the (clearly system-specific) dropbox line, just let me know.

--- build_mac.sh
+++ build_mac.sh
@@ -1,2 +1,8 @@
 pyinstaller playscii_mac.spec
+cd dist/Playscii.app/Contents/MacOS/
+ln -s libSDL2-2.0.0.dylib libSDL2.dylib
+ln -s libSDL2_mixer-2.0.0.dylib libSDL2_mixer.dylib
+cd ../../../../
+hdiutil create -srcfolder dist/Playscii.app/ playscii_mac-`cat version`.dmg
+mv playscii_mac-`cat version`.dmg ~/Dropbox/playscii/releases
 
