d0dfd55abeca6aa2b50f3a5842c2d38b803869c3
jplebreton <jplebreton@localhost>
2015-10-23 19:33:16 -0700

    - collision: don't call stopped_colliding if other isn't in object's contacts dict, but research why this happens - added GameRoom.camera_follow_player -- added GameWorld.enabled/disable player camera lock functions - created GameCamera subclass for any defaults that work better for game mode - message line gives feedback when object is click-spawned - removed world-to-screen debug element - stubbed out rooms and progression of maze game, let's call it an alpha :]

--- games/maze/scripts/rooms.py
+++ games/maze/scripts/rooms.py
@@ -1,5 +1,16 @@
 
-from game_room import GameRoom#, ObjectSpawnData
+from game_room import GameRoom
 
-class FirstAnimalRoom(GameRoom):
-    pass
+class OutsideRoom(GameRoom):
+    
+    camera_follow_player = True
+    
+    def entered(self, old_room):
+        GameRoom.entered(self, old_room)
+        self.world.collision_enabled = False
+        self.world.app.camera.y_tilt = 4
+    
+    def exited(self, new_room):
+        GameRoom.exited(self, new_room)
+        self.world.collision_enabled = True
+        self.world.app.camera.y_tilt = 0
