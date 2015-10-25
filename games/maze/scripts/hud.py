c98b77e4ac5cb5df0374686a7e1d34d62f7bc011
jplebreton <jplebreton@localhost>
2015-10-25 14:49:27 -0700

    maze: - sound for using portal key -- set portal key's consume_on_use False, else sound gets immediately cut off on its destruction.  need a proper fix for this - fixed hud message getting cut off - halved update rate of portal anim, looks better - closed up debug shortcut holes :]

--- games/maze/scripts/hud.py
+++ games/maze/scripts/hud.py
@@ -7,7 +7,7 @@ class MazeHUD(GameHUD):
     
     def __init__(self, world):
         GameHUD.__init__(self, world)
-        self.msg_art = self.world.app.new_art('mazehud_msg', 30, 1,
+        self.msg_art = self.world.app.new_art('mazehud_msg', 38, 1,
                                               'jpetscii', 'c64_original')
         self.msg = GameHUDRenderable(self.world.app, self.msg_art)
         self.arts = [self.msg_art]
