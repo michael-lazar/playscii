a4a81aaaca3b1178a1a1f700f57f73bea99db5e5
jplebreton <jplebreton@localhost>
2017-02-18 13:37:31 -0800

    - added plaintext exporter - relies on Charset.char_mapping data, so a charset's data file must support any extended characters you want in the output -- added most possible extended characters to DOS charset as an example - new palette: MSPaint (windows XP version) - import a few more modules in main so playscii.cfg can change their settings

---
+++ formats/out_txt.py
@@ -0,0 +1,28 @@
+from art_export import ArtExporter
+
+class TextExporter(ArtExporter):
+    format_name = 'Plain text'
+    format_description = """
+ASCII art in ordinary text format.
+Assumes single frame, single layer document.
+Current character set will be used; make sure it supports
+any extended characters you want translated.
+    """
+    file_extension = 'txt'
+    def run_export(self, out_filename, options):
+        outfile = open(out_filename, 'w')
+        for y in range(self.art.height):
+            for x in range(self.art.width):
+                char = self.art.get_char_index_at(0, 0, x, y)
+                found_char = False
+                for k,v in self.art.charset.char_mapping.items():
+                    if v == char:
+                        found_char = True
+                        outfile.write(k)
+                        break
+                # if char not found, just write a blank space
+                if not found_char:
+                    outfile.write(' ')
+            outfile.write('\n')
+        outfile.close()
+        return True
