6cfb7647e7c90e9812b9ca38ff042384e12c58cc
jplebreton <jplebreton@localhost>
2017-03-23 09:19:05 -0700

    - fix issue #50, proper FG transparency now. adds a branch to shaders/renderable_f.glsl though, must assess perf impact

--- shaders/renderable_f.glsl
+++ shaders/renderable_f.glsl
@@ -27,15 +27,21 @@ void main()
 	// offset U coord slightly so we're not sampling from pixel boundary
 	colorUV.x = (theFgColorIndex + 0.01) / palTextureWidth;
 	vec4 fgColor = texture(palette, colorUV);
-	// multiple charset pixel value by FG color
-	// tinting >1 color charsets isn't officially supported but hey
-	outColor.rgb *= fgColor.rgb;
-	// any totally transparent pixels get the BG color
 	colorUV.x = (theBgColorIndex + 0.01) / palTextureWidth;
 	vec4 bgColor = texture(palette, colorUV);
-	bgColor.a *= bgColorAlpha;
-	// thanks Mark Wonnacott for tip on how to do this w/o a branch
-	outColor = mix(bgColor, outColor, outColor.a);
+	// separate paths for full vs FG vs BG transparency
+    if ( theFgColorIndex < 0.99 && theBgColorIndex < 0.99 ) {
+	    outColor.rgba = vec4(0.0, 0.0, 0.0, 0.0);
+	} else if ( theFgColorIndex < 0.99 ) {
+	    // this produces a cut-out effect
+	    outColor.a = (1.0 - outColor.a) * bgColorAlpha;
+	    outColor.rgb = bgColor.rgb;
+	} else {
+	    outColor.rgb *= fgColor.rgb;
+	    bgColor.a *= bgColorAlpha;
+		// any totally transparent pixels in source get the BG color
+	    outColor = mix(bgColor, outColor, outColor.a);
+	}
 	// apply "grain" for eg UI elements
 	vec4 grainColor = texture(grain, gl_FragCoord.xy * grainSize);
 	outColor.rgb += (0.5 - grainColor.rgb) * grainStrength;
