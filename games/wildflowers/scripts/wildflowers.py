7154b44577ee287884352f59fc95bf505fa52e07
jplebreton <jplebreton@localhost>
2019-12-22 09:17:09 -0800

    - a bit of code cleanup, checkpointing before getting deeper in to this approach because the results are already kinda neat looking and it might be useful to refer back to this

--- games/wildflowers/scripts/wildflowers.py
+++ games/wildflowers/scripts/wildflowers.py
@@ -2,22 +2,6 @@
 from game_object import GameObject
 from game_util_objects import WorldGlobalsObject
 
-"""
-
-- size art to 8 x 8
-- jpetscii set and dpaint palette
--- support multiple palettes + ramps? eg doom, heretic, quake too. atari = vertical ascending ramps
-- clear (bg 0)
-
-- starting at bottom right, create N "fronds" that grow out towards edges
--- pick an end point, or roll randomly to see if it's finished?
-- define start (last index) of color ramps, increment through them as a frond grows
-- choose different, pre-defined characters - how? chains of character mutations?
-
-- when done, copy + mirror about X axis
-- then copy + mirror about Y axis to complete the 2-axis symmetry
-
-"""
 
 class FlowerGlobals(WorldGlobalsObject):
     
