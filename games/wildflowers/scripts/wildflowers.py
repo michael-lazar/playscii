73ea2b525e274fabd5be223b97b5b602ef7b12e4
jplebreton <jplebreton@localhost>
2019-12-22 18:15:03 -0800

    - test mode to generate a 4x4 grid of flowers - run generate script from flower's constructor - more fronds

--- games/wildflowers/scripts/wildflowers.py
+++ games/wildflowers/scripts/wildflowers.py
@@ -5,18 +5,31 @@ from game_util_objects import WorldGlobalsObject
 
 class FlowerGlobals(WorldGlobalsObject):
     
+    test_gen = True
+    
     def __init__(self, world, obj_data=None):
         WorldGlobalsObject.__init__(self, world, obj_data)
     
     def pre_first_update(self):
-        flower = FlowerObject(self.world)
-        flower.art.run_script('generate_flower')
-        self.world.camera.set_loc(flower.art_width / 2,
+        if self.test_gen:
+            for x in range(4):
+                for y in range(4):
+                    flower = self.world.spawn_object_of_class('FlowerObject')
+                    flower.set_loc(x * flower.art.width, y * flower.art.height)
+            self.world.camera.set_loc(30, 20, 35)
+        else:
+            flower = self.world.spawn_object_of_class('FlowerObject')
+            self.world.camera.set_loc(flower.art_width / 2,
                                   -flower.art_height / 2, 10)
-        self.flower = flower
+            self.flower = flower
 
 
 class FlowerObject(GameObject):
+    
     generate_art = True
     should_save = False
     physics_move = False
+    
+    def __init__(self, world, obj_data=None):
+        GameObject.__init__(self, world, obj_data)
+        self.art.run_script('generate_flower')
