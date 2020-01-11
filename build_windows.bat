64c37a3e43aa7d3afbe368a95933f9a7213b8796
jplebreton <jplebreton@localhost>
2020-01-11 15:28:37 -0800

    - Use PyInstaller "single file" building for Win32 builds - nicer to have a less messy base directory. Generates a .spec file on the fly and uses xcopy + copy for included files (palettes, example games, etc) instead of relying on PyInstaller for that.

--- build_windows.bat
+++ build_windows.bat
@@ -1,7 +1,24 @@
 @echo off
 
 echo Creating new build...
-pyinstaller playscii_win.spec
+pyinstaller -F playscii.py
+echo Build done!
 
+echo -----------
+
+echo Copying external files...
+REM xcopy dirs recursively
+for /f "tokens=*" %%i in (win_xcopy_include) DO (
+echo %%i
+xcopy /E/Y "%%i" "dist\%%i" /exclude:win_copy_exclude
+)
+REM regular copy files (non-recursively)
+for /f "tokens=*" %%i in (win_copy_include) DO (
+echo %%i
+copy /Y "%%i" "dist\" > NUL
+)
+
+echo -----------
 echo Done!
+
 pause
