22f9ddcc4baeb29b814c3104c298b50d007a9150
jplebreton <jplebreton@localhost>
2020-01-11 22:36:10 -0800

    win32 build: - fix pdoc generation failing by telling pyinstaller to include the .mako template files it needs - batch cleanup, use env variables to centralize important paths

--- build_windows.bat
+++ build_windows.bat
@@ -1,26 +1,40 @@
 @echo off
 
+set BUILD_EXE_PATH=dist\playscii.exe
+set OUTPUT_DIR=dist\playscii\
+set ICON_PATH=ui\playscii.ico
+set XCOPY_INCLUDE=win_xcopy_include
+set XCOPY_EXCLUDE=win_xcopy_exclude
+set COPY_INCLUDE=win_copy_include
+
 echo Creating new build...
+
+REM ==== include pdoc/mako templates from python site-packages - annoying!
+python -c "from site import getsitepackages; print(getsitepackages()[1])" > pypath
+set /p PYPATHSP=<pypath
+del pypath
+mkdir %OUTPUT_DIR%pdoc\templates\
+
 REM ==== -F = everything in one file; -w = no console window; -i = path to icon
-pyinstaller -F -w -i ui\playscii.ico playscii.py
+pyinstaller -F -w -i %ICON_PATH% --add-data %PYPATHSP%\pdoc\templates\*.mako;pdoc\templates\ playscii.py
 echo Build done!
 
 REM ==== move build so that ZIP will have a subdir enclosing everything
-mkdir dist\playscii
-move dist\playscii.exe dist\playscii\
+mkdir %OUTPUT_DIR%
+move %BUILD_EXE_PATH% %OUTPUT_DIR%
 
 echo -----------
 
 echo Copying external files...
 REM ==== xcopy dirs recursively
-for /f "tokens=*" %%i in (win_xcopy_include) DO (
+for /f "tokens=*" %%i in (%XCOPY_INCLUDE%) DO (
 echo %%i
-xcopy /E/Y "%%i" "dist\playscii\%%i" /exclude:win_copy_exclude
+xcopy /E/Y "%%i" "%OUTPUT_DIR%\%%i" /exclude:%XCOPY_EXCLUDE%
 )
 REM ==== regular copy files (non-recursively)
-for /f "tokens=*" %%i in (win_copy_include) DO (
+for /f "tokens=*" %%i in (%COPY_INCLUDE%) DO (
 echo %%i
-copy /Y "%%i" "dist\playscii\" > NUL
+copy /Y "%%i" %OUTPUT_DIR% > NUL
 )
 
 echo -----------
