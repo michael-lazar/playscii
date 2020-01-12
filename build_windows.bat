7e340b12090442cac64ffbae032978d2a03e2dfd
jplebreton <jplebreton@localhost>
2020-01-11 16:48:24 -0800

    - cleanup and comments for new windows build batch - win32 pyinstaller options: no console window, use the right icon

--- build_windows.bat
+++ build_windows.bat
@@ -1,21 +1,26 @@
 @echo off
 
 echo Creating new build...
-pyinstaller -F playscii.py
+REM ==== -F = everything in one file; -w = no console window; -i = path to icon
+pyinstaller -F -w -i ui\playscii.ico playscii.py
 echo Build done!
+
+REM ==== move build so that ZIP will have a subdir enclosing everything
+mkdir dist\playscii
+move dist\playscii.exe dist\playscii\
 
 echo -----------
 
 echo Copying external files...
-REM xcopy dirs recursively
+REM ==== xcopy dirs recursively
 for /f "tokens=*" %%i in (win_xcopy_include) DO (
 echo %%i
-xcopy /E/Y "%%i" "dist\%%i" /exclude:win_copy_exclude
+xcopy /E/Y "%%i" "dist\playscii\%%i" /exclude:win_copy_exclude
 )
-REM regular copy files (non-recursively)
+REM ==== regular copy files (non-recursively)
 for /f "tokens=*" %%i in (win_copy_include) DO (
 echo %%i
-copy /Y "%%i" "dist\" > NUL
+copy /Y "%%i" "dist\playscii\" > NUL
 )
 
 echo -----------
