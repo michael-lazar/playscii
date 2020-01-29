f8141b52a97b7f0e06f60afcccb604dc0129e9c7
jplebreton <jplebreton@localhost>
2020-01-29 12:31:11 -0800

    wildflowers: - moved flower generation entirely out of the artscript into its own class - moved palette ramp definitions into their own file - make "mirrored drawing" its own function as petals will probably call it - added some recent notes

---
+++ games/wildflowers/scripts/ramps.py
@@ -0,0 +1,95 @@
+
+# wildflowers palette ramp definitions
+
+PALETTE_RAMPS = {
+    # palette name : list of its ramps
+    'dpaint': [
+        # ramp tuple: (start index, length, stride)
+        # generally, lighter / more vivid to darker
+        (17, 16, 1),  # white to black
+        (33, 16, 1),  # red to black
+        (49, 8, 1),   # white to red
+        (57, 8, 1),   # light orange to dark orange
+        (65, 16, 1),  # light yellow to ~black
+        (81, 8, 1),   # light green to green
+        (89, 24, 1),  # white to green to ~black
+        (113, 16, 1), # light cyan to ~black
+        (129, 8, 1),  # light blue to blue
+        (137, 24, 1), # white to blue to ~black
+        (161, 16, 1), # light purple to ~black
+        (177, 16, 1), # light magenta to ~black
+        (193, 24, 1), # pale flesh to ~black
+        (225, 22, 1)  # ROYGBV rainbow
+    ],
+    'doom': [
+        (17, 27, 1),  # very light pink to dark red
+        (44, 20, 1),  # pale flesh to brown
+        (69, 26, 1),  # white to very dark grey
+        (95, 14, 1),  # bright green to ~black
+        (109, 12, 1), # light tan to dark tan
+        (126, 4, 1),  # olive drab
+        (130, 7, 1),  # light gold to gold brown
+        (137, 18, 1), # white to dark red
+        (155, 14, 1), # white to dark blue
+        (169, 11, 1), # white to orange
+        (180, 7, 1),  # white to yellow
+        (187, 4, 1),  # orange to burnt orange
+        (193, 7, 1),  # dark blue to black
+        (201, 5, 1)   # light magenta to dark purple
+    ],
+    'quake': [
+        (16, 15, -1),  # white to black
+        (32, 16, -1),  # mustard to black
+        (48, 16, -1),  # lavender to black
+        (63, 15, -1),  # olive to black
+        (79, 16, -1),  # red to black
+        (92, 13, -1),  # orange to ~black
+        (108, 16, -1), # yellow to orange to ~black
+        (124, 16, -1), # pale flesh to ~black
+        (125, 16, 1),  # light purple to ~black
+        (141, 13, 1),  # purpleish pink to ~black
+        (154, 15, 1),  # light tan to ~black
+        (169, 16, 1),  # light olive to ~black
+        (185, 14, 1),  # yellow to ~black
+        (199, 31, 1),  # blue to black to light orange
+        (233, 4, -1),  # yellow to brown
+        (236, 3, -1),  # light blue to blue
+        (240, 4, -1),  # red to dark red
+        (243, 3, -1)   # white to yellow
+    ],
+    'heretic': [
+        (35, 35, -1),  # white to black
+        (51, 16, -1),  # light grey to dark grey
+        (65, 14, -1),  # white to dark violent-grey
+        (94, 29, -1),  # white to dark brown
+        (110, 16, -1), # light tan to brown
+        (136, 26, -1), # light yellow to dark golden brown
+        (144, 8, -1),  # yellow to orange
+        (160, 16, -1), # red to dark red
+        (168, 8, -1),  # white to pink
+        (176, 8, -1),  # light magenta to dark magenta
+        (184, 8, -1),  # white to purple
+        (208, 24, -1), # white to cyan to dark blue
+        (224, 16, -1), # light green to dark green
+        (240, 16, -1), # olive to dark olive
+        (247, 7, -1)   # red to yellow
+    ],
+    'atari': [
+        (113, 8, -16), # white to black
+        (114, 8, -16), # yellow to muddy brown
+        (115, 8, -16), # dull gold to brown
+        (116, 8, -16), # peach to burnt orange
+        (117, 8, -16), # pink to red
+        (118, 8, -16), # magenta to dark magenta
+        (119, 8, -16), # purple to dark purple
+        (120, 8, -16), # violet to dark violet
+        (121, 8, -16), # light blue to dark blue
+        (122, 8, -16), # light cobalt to dark cobalt
+        (123, 8, -16), # light teal to dark teal
+        (124, 8, -16), # light sea green to dark sea green
+        (125, 8, -16), # light green to dark green
+        (126, 8, -16), # yellow green to dark yellow green
+        (127, 8, -16), # pale yellow to dark olive
+        (128, 8, -16)  # gold to golden brown
+    ]
+}
--- games/wildflowers/scripts/wildflowers.py
+++ games/wildflowers/scripts/wildflowers.py
@@ -1,6 +1,32 @@
 
-from game_object import GameObject
 from game_util_objects import WorldGlobalsObject
+
+from games.wildflowers.scripts.flower import FlowerObject
+
+"""
+2020-01-28 notes:
+
+"frond style" - different evolution step functions that produce different shapes, eg curls/spirals, right angles, sawtooths, diagonal or straight lines, etc.
+- each style iterator should log and refer to steps it's already done, eg for curls turn clockwise. "if the last bud was up and left, either keep going up and left, or go straight up; if the last bud was up, either keep going up or go up and right; etc
+- one style: frond draws each growth dir from a deck, reshuffling when empty to avoid repetition?
+- possibly optional: some fronds are "pure" ones of each style, others mix between styles depending on age (eg first half curl, second half straight line?)
+- one style weights growth dirs differently depending on its remaining life?
+
+each frond should have a "chaos" value that determines how frequently it changes its character.
+
+random range of start locations? fronds not connected to the center would be nice. maybe weight in a radius, ie likely to start from center, but rarely can start further and further away.
+
+approaches to generate solid color shapes beneath (before) the linework? blocks, diamonds. these need a whole separate generation function(s).
+- do diamonds imply a particular character used?
+
+2019-12-22 ideas for future development:
+- character ramps based on direction changes, visual density, something else?
+- strategies for sometimes filling in tile BG colors? quarter circles or other patterns that mesh interestingly with char/fg details?
+-- use ramps, fade over distance from center in a circle, diamond, square etc pattern
+alt generation methods:
+https://terbium.io/2018/11/wave-function-collapse
+https://www.conwaylife.com/wiki/Conway%27s_Game_of_Life
+"""
 
 
 class FlowerGlobals(WorldGlobalsObject):
@@ -20,17 +46,5 @@ class FlowerGlobals(WorldGlobalsObject):
             self.world.camera.set_loc(30, 20, 35)
         else:
             flower = self.world.spawn_object_of_class('FlowerObject')
-            self.world.camera.set_loc(flower.art_width / 2,
-                                  -flower.art_height / 2, 10)
+            self.world.camera.set_loc(0, 0, 10)
             self.flower = flower
-
-
-class FlowerObject(GameObject):
-    
-    generate_art = True
-    should_save = False
-    physics_move = False
-    
-    def __init__(self, world, obj_data=None):
-        GameObject.__init__(self, world, obj_data)
-        self.art.run_script('generate_flower')
